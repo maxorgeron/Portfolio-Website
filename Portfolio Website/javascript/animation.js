d697ceabe98ee1d9e6c81063a3f4f167e0d95ad6
maxorgeron <77476090+maxorgeron@users.noreply.github.com>
2023-10-06 12:56:46 -0400

    Add files via upload

---
+++ Portfolio Website/javascript/animation.js
@@ -0,0 +1,44 @@
+
+/* Animation Portfolio 
+---------------------------------------------------------------------------------
+
+
+document.getElementById("pv-1").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("pv-1").addEventListener("mouseleave", function() {
+	this.pause();
+});
+
+
+document.getElementById("pv-2").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("pv-2").addEventListener("mouseleave", function() {
+	this.pause();
+});
+
+
+document.getElementById("pv-3").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("pv-3").addEventListener("mouseleave", function() {
+	this.pause();
+});
+
+
+document.getElementById("pv-4").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("pv-4").addEventListener("mouseleave", function() {
+	this.pause();
+});
+
+*/
+
+
+
---
+++ Portfolio Website/javascript/vfx.js
@@ -0,0 +1,67 @@
+
+
+/* VFX Portfolio 
+---------------------------------------------------------------------------------
+
+
+document.getElementById("ps-1").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("ps-1").addEventListener("mouseleave", function() {
+	this.pause();
+});
+
+document.getElementById("ps-2").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("ps-2").addEventListener("mouseleave", function() {
+	this.pause();
+});
+
+document.getElementById("ps-3").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("ps-3").addEventListener("mouseleave", function() {
+	this.pause();
+});
+
+
+document.getElementById("ps-4").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("ps-4").addEventListener("mouseleave", function() {
+	this.pause();
+});
+
+
+document.getElementById("ps-5").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("ps-5").addEventListener("mouseleave", function() {
+	this.pause();
+});
+
+
+document.getElementById("ps-6").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("ps-6").addEventListener("mouseleave", function() {
+	this.pause();
+});
+
+
+document.getElementById("ps-9").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("ps-9").addEventListener("mouseleave", function() {
+	this.pause();
+});
+
+*/
